ed0ce59bf8e702e106c06a98c36fbaab9592d917
yoshifumi.fujimoto <yoshifumi.fujimoto@alphacode.co.jp>
2019-09-11 20:43:20 +0900

    テストマップ追加

---
+++ _bundle/assets/map/tile_a.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileA" tilewidth="32" tileheight="32" tilecount="240" columns="16">
+ <image source="tile_a.png" width="512" height="480"/>
+</tileset>
---
+++ _bundle/assets/map/tile_b.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tile_b" tilewidth="32" tileheight="32" tilecount="192" columns="16">
+ <image source="tile_b.png" width="512" height="384"/>
+</tileset>
